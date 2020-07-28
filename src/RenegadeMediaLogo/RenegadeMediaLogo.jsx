b0221e53c936648973905e6d5c1bc6458af0db38
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-28 16:32:52 -0400

    yay

---
+++ src/RenegadeMediaLogo/RenegadeMediaLogo.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./RenegadeMediaLogo.css";
+
+function RenegadeMediaLogo() {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="cracked">
+          <span>Renegade&nbsp;</span>
+          <span>Renegade&nbsp;</span>
+          <span>Renegade&nbsp;</span>
+        </div>
+        <div className="cracked">
+          <span>Media</span>
+          <span>Media</span>
+          <span>Media</span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RenegadeMediaLogo;
