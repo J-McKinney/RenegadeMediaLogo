ff670b3b9a22c364fb8b353e6b8544cc3890790d
Jesse McKinney <jessemckinney513@gmail.com>
2020-07-28 16:12:28 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import RMLogo from "./RenegadeMediaLogo/RenegadeMediaLogo";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
